25e2ca16bc3b453f7a02f025d972bbfdecc9df11
anzen-sato <85572165+anzen-sato@users.noreply.github.com>
2021-06-09 23:35:25 +0300

    add Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,11 +2,12 @@ import {useState} from "react";
 const Categories = (props) => {
     const selectItem = (item) => {
         setSelectedItem(item);
+        console.log(item)
     }
     const [selectedItem, setSelectedItem] = useState(null);
     return <div className="categories">
               <ul>
-                <li className={selectedItem === null ? "active" : ""}>Все</li>
+                <li onClick={()=>selectItem(null)} className={selectedItem === null ? "active" : ""}>Все</li>
                   {
                       props.items && props.items.map((item, index) =>
                           <li className={selectedItem === item ? "active" : ""} onClick={()=>selectItem(item)} key={`${item}_${index}`}>{item}</li>)
