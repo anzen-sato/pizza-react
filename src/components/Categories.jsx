0fe1b6b0e9db69ea8a3f91ff4f7b2b6981febdbd
anzen-sato <85572165+anzen-sato@users.noreply.github.com>
2021-06-10 00:10:54 +0300

    add sortPopup

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,8 @@
 import {useState} from "react";
 const Categories = (props) => {
-    const selectItem = (item) => {
-        setSelectedItem(item);
-        console.log(item)
+    const selectItem = (index) => {
+        setSelectedItem(index);
+        console.log(index)
     }
     const [selectedItem, setSelectedItem] = useState(null);
     return <div className="categories">
@@ -10,7 +10,7 @@ const Categories = (props) => {
                 <li onClick={()=>selectItem(null)} className={selectedItem === null ? "active" : ""}>Все</li>
                   {
                       props.items && props.items.map((item, index) =>
-                          <li className={selectedItem === item ? "active" : ""} onClick={()=>selectItem(item)} key={`${item}_${index}`}>{item}</li>)
+                          <li className={selectedItem === index ? "active" : ""} onClick={()=>selectItem(index)} key={`${item}_${index}`}>{item}</li>)
                   }
               </ul>
             </div>
