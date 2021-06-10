2d11fcd771be0ba18b0a30ad410bf6e048230357
anzen-sato <85572165+anzen-sato@users.noreply.github.com>
2021-06-10 21:49:37 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { Header } from "./components";
 import logo from "./img/pizza-logo.svg";
 import { Cart, Home } from "./pages";
 import { Route } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/db.json")
       .then((res) => res.json())
-      .then((json) => console.log(json));
+      .then((json) => setPizzas(json.pizzas));
   }, []);
   return (
     <div className="wrapper">
       <Header logo={logo} />
-      <Route path={"/"} component={Home} exact />
-      <Route path={"/cart"} component={Cart} exact />
+      <Route path={"/"} render={() => <Home items={pizzas} />} exact />
+      <Route path={"/cart"} render={() => <Cart />} exact />
     </div>
   );
 }
