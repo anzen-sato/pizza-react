851652796966d3212e2a82c02d5a15ff876bf61b
anzen-sato <85572165+anzen-sato@users.noreply.github.com>
2021-06-10 13:26:48 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route exact path={"/"} component={App} />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
